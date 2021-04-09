8a8087ce5d547ad04f493fdb8403563e1846b807
Hoang Nguyen <20176020@sie.edu.vn>
2021-04-09 11:53:29 +0700

    Temp save

--- domain/services/productService.js
+++ domain/services/productService.js
@@ -1,122 +1,153 @@
 var Product = require("../models/product");
 
 const productService = {
-  getProduct: async (productID) => {
-    let result = await Product.findById(productID);
-    if (result) {
-      return result;
-    } else {
-      throw new Error("error/PRODUCT_NOT_FOUND/WRONG_ID");
-    }
-  },
-  getAllProduct: async () => {
-    let result = await Product.find();
-    if (result && result.length != 0) {
-      return result;
-    } else {
-      throw new Error("error/CANNOT_GET_ALL/UNIDENTIFY_ERROR");
-    }
-  },
-  getFilteredProduct: async ({
-    productCategory,
-    gender,
-    size,
-    price,
-    tags,
-    year,
-    sort
-  }, offset = 0, limit = 5) => {
-    let andQueries = [];
-    for (const [key, value] of Object.entries({ productCategory, gender, size, price, tags, year })) {
-      if (value != null) {
-        if (key === 'size') andQueries = [{ sizeQuantity: { $elemMatch: { size: { $eq: value }, quantity: { $ne: 0 } } } }, ...andQueries];
-        else if (key === 'price') {
-          let priceQuery = [];
-          value.split(",").map(i => {
-            if (i.includes("<")) priceQuery = [{ [key]: { $lte: parseInt(i.replace("<", "")) } }, ...priceQuery];
-            else if (i.includes("-")) priceQuery = [{ $and: [{ [key]: { $gte: parseInt(i.split("-")[0]) } }, { [key]: { $lte: parseInt(i.split("-")[1]) } }] }, ...priceQuery];
-            else if (i.includes(">")) priceQuery = [{ [key]: { $gte: parseInt(i.replace(">", "")) } }, ...priceQuery];
-          })
-          andQueries = [{ $or: [...priceQuery] }, ...andQueries];
+    getProduct: async (productID) =>
+    {
+        let result = await Product.findById(productID);
+        if (result)
+        {
+            return result;
+        } else
+        {
+            throw new Error("error/PRODUCT_NOT_FOUND/WRONG_ID");
         }
-        else if (key === 'tags') andQueries = [{ tags: { $all: value.split(",") } }, ...andQueries];
-        else if (key === 'year') {
-          let yearQuery = [];
-          value.split(",").map(i => {
-            yearQuery = [
-              {
-                releaseDate: {
-                  $gte: new Date(new Date(i, 0, 2).setHours(00, 00, 00)),
-                  $lte: new Date(new Date(i, 11, 31).setHours(23, 59, 59))
-                }
-              }, ...yearQuery]
-          });
-          andQueries = [{ $or: [...yearQuery] }, ...andQueries];
+    },
+    getAllProduct: async () =>
+    {
+        let result = await Product.find();
+        if (result && result.length != 0)
+        {
+            return result;
+        } else
+        {
+            throw new Error("error/CANNOT_GET_ALL/UNIDENTIFY_ERROR");
         }
-        else andQueries = [{ [key]: value }, ...andQueries];
-      }
-    }
-    const searchQueries = { $and: [...andQueries] };
-    console.log(searchQueries);
-    const totalRecord = await Product.countDocuments(searchQueries);
-    const result = await Product.find(searchQueries).limit(parseInt(limit)).skip(parseInt(limit) * parseInt(offset));
-    if (result) {
-      return { totalRecord, result };
-    } else {
-      throw new Error("error/CANNOT_GET_FILTERED_PRODUCTS/UNEXPECTED_QUERIES");
-    }
-  },
-  createProduct: async (ProductOps) => {
-    if (ProductOps["productName"] && ProductOps["price"] && ProductOps["sizeQuantity"]) {
-      const result = await Product.findOne({ productName: ProductOps["productName"] });
-      if (!result) {
-        const newProduct = Product(ProductOps);
-        await newProduct.save();
-        return newProduct;
-      } else {
-        throw new Error("error/PRODUCT_ALREADY_CREATED");
-      }
-    } else {
-      throw new Error("error/PRODUCT_LACK_INFO");
-    }
-  },
-  updateProduct: async (_id, updateOps) => {
-    updateOps["dateUpdated"] = Date.now();
-    // const {sizeQuantity, tags, ...otherOps} = updateOps;
-    // console.log("Quantiti", sizeQuantity,"Tags", tags,"Other", otherOps)
-    // updateOps["dateUpdated"] = Date.now();
-    // const {sizeQuantity, tags, ...otherOps} = updateOps;
-    // let product = await Product.findById(_id);
-    // for (i in sizeQuantity){
-    //   product.sizeQuantity.push(sizeQuantity[i])
-    // }
-    // for (i in tags){
-    //   product.tags.push(tags[i])
-    // }
-    // await product.updateOne(otherOps);
-    // let result = await product.save()
-    // console.log(result)
-    // if (result) {
-    //   return result;
-    // } else {
-    //   throw new Error("error/PRODUCT_NOT_FOUND/WRONG_ID");
-    // }
-    let result = await Product.updateOne({ _id }, { $set: updateOps });
-    if (result) {
-      return result;
-    } else {
-      throw new Error("error/PRODUCT_NOT_FOUND/WRONG_ID");
-    }
-  },
+    },
+    getFilteredProduct: async ({
+                                   productCategory,
+                                   gender,
+                                   size,
+                                   price,
+                                   tags,
+                                   year,
+                                   sort
+                               }, offset = 0, limit = 5) =>
+    {
+        let andQueries = [];
+        for (const [key, value] of Object.entries({productCategory, gender, size, price, tags, year}))
+        {
+            if (value != null)
+            {
+                if (key === "size") andQueries = [{
+                    sizeQuantity: {
+                        $elemMatch: {
+                            size: {$eq: value},
+                            quantity: {$ne: 0}
+                        }
+                    }
+                }, ...andQueries];
+                else if (key === "price")
+                {
+                    let priceQuery = [];
+                    value.split(",").map(i =>
+                    {
+                        if (i.includes("<")) priceQuery = [{[key]: {$lte: parseInt(i.replace("<", ""))}}, ...priceQuery];
+                        else if (i.includes("-")) priceQuery = [{$and: [{[key]: {$gte: parseInt(i.split("-")[0])}}, {[key]: {$lte: parseInt(i.split("-")[1])}}]}, ...priceQuery];
+                        else if (i.includes(">")) priceQuery = [{[key]: {$gte: parseInt(i.replace(">", ""))}}, ...priceQuery];
+                    });
+                    andQueries = [{$or: [...priceQuery]}, ...andQueries];
+                } else if (key === "tags") andQueries = [{tags: {$all: value.split(",")}}, ...andQueries];
+                else if (key === "year")
+                {
+                    let yearQuery = [];
+                    value.split(",").map(i =>
+                    {
+                        yearQuery = [
+                            {
+                                releaseDate: {
+                                    $gte: new Date(new Date(i, 0, 2).setHours(00, 00, 00)),
+                                    $lte: new Date(new Date(i, 11, 31).setHours(23, 59, 59))
+                                }
+                            }, ...yearQuery];
+                    });
+                    andQueries = [{$or: [...yearQuery]}, ...andQueries];
+                } else andQueries = [{[key]: value}, ...andQueries];
+            }
+        }
+        const searchQueries = {$and: [...andQueries]};
+        console.log(searchQueries);
+        const totalRecord = await Product.countDocuments(searchQueries);
+        const result = await Product.find(searchQueries).limit(parseInt(limit)).skip(parseInt(limit) * parseInt(offset));
+        if (result)
+        {
+            return {totalRecord, result};
+        } else
+        {
+            throw new Error("error/CANNOT_GET_FILTERED_PRODUCTS/UNEXPECTED_QUERIES");
+        }
+    },
+    createProduct: async (ProductOps) =>
+    {
+        if (ProductOps["productName"] && ProductOps["price"] && ProductOps["sizeQuantity"])
+        {
+            const result = await Product.findOne({productName: ProductOps["productName"]});
+            if (!result)
+            {
+                const newProduct = Product(ProductOps);
+                await newProduct.save();
+                return newProduct;
+            } else
+            {
+                throw new Error("error/PRODUCT_ALREADY_CREATED");
+            }
+        } else
+        {
+            throw new Error("error/PRODUCT_LACK_INFO");
+        }
+    },
+    updateProduct: async (_id, updateOps) =>
+    {
+        updateOps["dateUpdated"] = Date.now();
+        // const {sizeQuantity, tags, ...otherOps} = updateOps;
+        // console.log("Quantiti", sizeQuantity,"Tags", tags,"Other", otherOps)
+        // updateOps["dateUpdated"] = Date.now();
+        // const {sizeQuantity, tags, ...otherOps} = updateOps;
+        // let product = await Product.findById(_id);
+        // for (i in sizeQuantity){
+        //   product.sizeQuantity.push(sizeQuantity[i])
+        // }
+        // for (i in tags){
+        //   product.tags.push(tags[i])
+        // }
+        // await product.updateOne(otherOps);
+        // let result = await product.save()
+        // console.log(result)
+        // if (result) {
+        //   return result;
+        // } else {
+        //   throw new Error("error/PRODUCT_NOT_FOUND/WRONG_ID");
+        // }
+        let result = await Product.updateOne({_id}, {$set: updateOps});
+        if (result)
+        {
+            return result;
+        } else
+        {
+            throw new Error("error/PRODUCT_NOT_FOUND/WRONG_ID");
+        }
+    },
 
-  deleteProduct: async (_id) => {
-    let result = await Product.findOneAndRemove({ _id });
-    if (result) {
-      return result;
-    } else {
-      throw new Error("error/PRODUCT_NOT_FOUND/WRONG_ID");
+    deleteProduct: async (_id) =>
+    {
+        let result = await Product.findOneAndRemove({_id});
+        if (result)
+        {
+            return result;
+        } else
+        {
+            throw new Error("error/PRODUCT_NOT_FOUND/WRONG_ID");
+        }
     }
-  },
 };
 
 module.exports = productService;
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require("dotenv").config()
+require("dotenv").config();
 var express = require("express");
 var cors = require("cors");
-var router = require("./application/router/index.js") // mac dinh no se tro? toi index.js
-var morgan = require("morgan")
+var router = require("./application/router/index.js"); // mac dinh no se tro? toi index.js
+var morgan = require("morgan");
 var bodyParser = require("body-parser");
 
 // const expressSession = require('express-session')({
@@ -20,22 +20,25 @@ const app = express();
 // app.use(passport.initialize());
 // app.use(passport.session());
 
-require("./config/passport")
+require("./config/passport");
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended : true})) 
-app.use(morgan("dev"))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("dev"));
 app.use(cors());
 // app.use(expressSession)
 
 
-app.use("/api",router);
-app.get("/" , (req,res)=>{
+app.use("/api", router);
+app.get("/", (req, res) =>
+{
     res.json({
-        hello : "world"
-    })
-})
+        hello: "world"
+    });
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>{ console.log(`Running on port ${PORT}`)
-})
+app.listen(PORT, () =>
+{
+    console.log(`Running on port ${PORT}`);
+});
